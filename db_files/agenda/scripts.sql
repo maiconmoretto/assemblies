c2ad278bc31674b6effbffdf70b5881fd2718af2
maiconmoretto <maiconmorettos@gmail.com>
2020-05-26 17:53:28 -0300

    Added scripts

--- db_files/agenda/scripts.sql
+++ db_files/agenda/scripts.sql
@@ -1,2 +1,8 @@
 SELECT * FROM assemblies.agenda;
-DELETE FROM  assemblies.agenda;
+SELECT * FROM assemblies.agenda WHERE created_at  <= NOW() - INTERVAL 1 MINUTE;
+
+/*DELETE FROM  assemblies.agenda;*/
+
+SELECT DATE_FORMAT(create_at, '%m/%d/%Y %H:%i') as data_formatada FROM assemblies.agenda ;
+
+SELECT DATE_FORMAT(create_at, '%d/%m/%Y') FROM assemblies.agenda;
