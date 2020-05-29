6483a91a1c953bee6de926d0324b1c74d6528e8f
maiconmoretto <maiconmorettos@gmail.com>
2020-05-29 18:07:30 -0300

    Added deleteById and add test method in repository test

--- src/test/java/com/br/UserRepositoryTest.java
+++ src/test/java/com/br/UserRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.br;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -20,25 +22,41 @@ import com.br.repository.UserRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserRepositoryTest {
-	
+
 	@Mock
 	private UserRepository userRepository;
-	
+
 	@Before
-	public void setup(){
+	public void setup() {
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
 	@Test
-	public void getAll(){
+	public void findAll() {
 		List<User> userList = new ArrayList<User>();
 		userList.add(new User("user 1", "01-01-01"));
 		userList.add(new User("user 2", "02-02-02"));
-		userList.add(new User("user 3","01-03-03"));
+		userList.add(new User("user 3", "01-03-03"));
 		when(userRepository.findAll()).thenReturn(userList);
-		
+
 		List<User> result = userRepository.findAll();
 		assertEquals(3, result.size());
+	}
+
+	@Test
+	public void deleteById() {
+		User user = new User("User 1", "01-01-01");
+		userRepository.deleteById(user.getId());
+		verify(userRepository, times(1)).deleteById(user.getId());
+	}
+
+	@Test
+	public void add() {
+		User user = new User("User 1", "01-01-01");
+		when(userRepository.save(user)).thenReturn(user);
+		User result = userRepository.save(user);
+		assertEquals("User 1", result.getName());
+		assertEquals("01-01-01", result.getCreatedAt());
 	}
 
 }
