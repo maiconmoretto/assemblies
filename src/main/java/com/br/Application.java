10a97cf7ff4fbdb5a329add24eb9e75993c8815d
maiconmoretto <maiconmorettos@gmail.com>
2020-06-07 11:38:59 -0300

    Message sent

--- src/main/java/com/br/Application.java
+++ src/main/java/com/br/Application.java
@@ -1,23 +1,12 @@
 package com.br;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
  
 @SpringBootApplication
 public class Application {
- 
-    @Value("${queue.order.name}")
-    private String orderQueue;
- 
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
- 
-    @Bean
-    public Queue queue() {
-        return new Queue(orderQueue, true);
     }
 }
